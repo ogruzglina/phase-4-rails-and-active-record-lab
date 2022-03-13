9690ca9428a0faedf72b0f9bc6b9ee9508da22c7
Olha Gruzglina <olga.zhmura@gmail.com>
2022-03-13 15:19:26 -0400

    commit tree 72be772135e50973e58f3177827b493266cc3859

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+name = Faker::Name.first_name
+last_name = Faker::Name.last_name
+grade = rand(1..10)
+
+Student.create(first_name: name, last_name: last_name, grade: grade)
+end
+
